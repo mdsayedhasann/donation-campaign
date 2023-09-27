aebbc0d46fbdcfb199f7a9f6582822a8e935e5bb
sayed_hossain <sayed.hasan0011@gmail.com>
2023-09-27 19:51:35 +0600

    Statistics Page fully complete

--- src/components/Statistics/Statistics.jsx
+++ src/components/Statistics/Statistics.jsx
@@ -1,50 +1,61 @@
 /* eslint-disable react/jsx-no-undef */
-import React from 'react';
-import { useEffect } from 'react';
-import { useState } from 'react';
-import { useLoaderData } from 'react-router-dom';
-import { PieChart, Pie, Cell } from 'recharts';
-import { getDonations } from '../../utility/localStorage';
+import React from "react";
+import { useEffect } from "react";
+import { useState } from "react";
+import { useLoaderData } from "react-router-dom";
+import { PieChart, Pie, Cell } from "recharts";
+import { getDonations } from "../../utility/localStorage";
 
 const Statistics = () => {
-     const loadData = useLoaderData()
-     const [totalDonate, setTotalDonated] = useState([])
+  const loadData = useLoaderData();
+  const [totalDonate, setTotalDonated] = useState([]);
 
-    useEffect(() => {
-        const localStoragerData = getDonations();
-        if(loadData.length > 0){
-            const donatedData = loadData.filter(donate => localStoragerData.includes(donate.id))
-            setTotalDonated(donatedData)
-        }
-    },[])
-      
+  useEffect(() => {
+    const localStoragerData = getDonations();
+    if (loadData.length > 0) {
+      const donatedData = loadData.filter((donate) =>
+        localStoragerData.includes(donate.id)
+      );
+      setTotalDonated(donatedData);
+    }
+  }, []);
 
+  const data = [
+    { name: "Group A", value: loadData.length - totalDonate.length },
+    { name: "Group B", value: totalDonate.length },
+  ];
 
-    const data = [
-        { name: 'Group A', value: loadData.length - totalDonate.length},
-        { name: 'Group B', value: totalDonate.length }
-      ];
+  const COLORS = ["#FF444A", "#00C49F"];
 
+  const RADIAN = Math.PI / 180;
+  const renderCustomizedLabel = ({
+    cx,
+    cy,
+    midAngle,
+    innerRadius,
+    outerRadius,
+    percent,
+    index,
+  }) => {
+    const radius = innerRadius + (outerRadius - innerRadius) * 0.5;
+    const x = cx + radius * Math.cos(-midAngle * RADIAN);
+    const y = cy + radius * Math.sin(-midAngle * RADIAN);
 
-      
-      const COLORS = ['#FF444A', '#00C49F'];
-      
-      const RADIAN = Math.PI / 180;
-      const renderCustomizedLabel = ({ cx, cy, midAngle, innerRadius, outerRadius, percent, index }) => {
-        const radius = innerRadius + (outerRadius - innerRadius) * 0.5;
-        const x = cx + radius * Math.cos(-midAngle * RADIAN);
-        const y = cy + radius * Math.sin(-midAngle * RADIAN);
-      
-        return (
-          <text x={x} y={y} fill="white" textAnchor={x > cx ? 'start' : 'end'} dominantBaseline="central">
-            {`${(percent * 100).toFixed(0)}%`}
-          </text>
-        );
-      };
     return (
-        <div className='max-w-7xl mx-auto'>
-           <div className='flex justify-center text-center items-center'>
-          
+      <text
+        x={x}
+        y={y}
+        fill="white"
+        textAnchor={x > cx ? "start" : "end"}
+        dominantBaseline="central"
+      >
+        {`${(percent * 100).toFixed(0)}%`}
+      </text>
+    );
+  };
+  return (
+    <div className="max-w-7xl mx-auto">
+      <div className="flex justify-center text-center items-center">
         <PieChart width={400} height={400}>
           <Pie
             data={data}
@@ -57,13 +68,28 @@ const Statistics = () => {
             dataKey="value"
           >
             {data.map((entry, index) => (
-              <Cell key={`cell-${index}`} fill={COLORS[index % COLORS.length]} />
+              <Cell
+                key={`cell-${index}`}
+                fill={COLORS[index % COLORS.length]}
+              />
             ))}
           </Pie>
         </PieChart>
-           </div>
+      </div>
+
+      <div className="flex justify-center gap-16">
+        <div className="flex items-center gap-4">
+          <h3>Your Donation</h3>
+          <div className="bg-[#00C49F] h-3 w-40"></div>
         </div>
-    );
+
+        <div className="flex items-center gap-4">
+          <h3>Total Donation</h3>
+          <div className="bg-[#FF444A] h-3 w-40"></div>
+        </div>
+      </div>
+    </div>
+  );
 };
 
 export default Statistics;
